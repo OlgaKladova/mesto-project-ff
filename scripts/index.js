000b2ab3361539e3685a8016787a85317099afb4
Olga Kladova <Olenka-29-04@yandex.ru>
2024-03-13 16:14:48 +0300

    готово к проверке

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector('.places__list'); 
+
+function createCard() {
+    const cardTemplate = document.querySelector('#card-template').content;
+    
+    initialCards.forEach((place) => {
+        const card = cardTemplate.querySelector('.card').cloneNode(true);
+     
+        card.querySelector('.card__image').src = place.link;
+        card.querySelector('.card__title').textContent = place.name;
+        card.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+        
+        function deleteCard() {
+            card.remove(); 
+        }
+
+        placesList.append(card);
+    });
+
+    return;
+}
+
+createCard();
+   
